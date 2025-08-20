3e83470547fe242279d17c1725e67baf66a59830
Angelica <angelicamld@gmail.com>
2025-08-20 16:41:51 +0200

    adding basic header

---
+++ public/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/public/css/header.css">
+</head>
+<body>
+    <section class="header">
+        <img src="/public/images/logo.jpg" class="logo" alt="logo"/>
+        <h1>MyFavBreweries</h1>
+            <div class="button">
+                <button type="button">Inscription</button>
+                <button type="button">Connexion</button>
+            </div>
+    </section>
+</body>
+</html>
